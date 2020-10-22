3a6ad3025c7679cf5ce783307f8443cf9871ad77
qaqasazeri <fagansafarov1985@gmail.com>
2020-10-21 22:27:17 -0500

    Spring-DI-SetterInjection

---
+++ Spring-02-DI-SetterInjection/src/main/java/com/cybertek/services/OfficeHours.java
@@ -0,0 +1,10 @@
+package com.cybertek.services;
+
+import interfaces.ExtraSession;
+
+public class OfficeHours implements ExtraSession {
+
+    public int getOfficeHOurs() {
+        return 11;
+    }
+}
