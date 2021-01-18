ec51a93f1b897a39f9e979ecd8e716d49179f73d
fagansafarov <fagansafarov1985@gmail.com>
2021-01-17 21:53:57 -0600

    Security-HttpBasic-Database

---
+++ Spring-10-Security-HttpBasic-Datasbase/src/main/java/com/cybertek/bootstrap/DataGenerator.java
@@ -0,0 +1,37 @@
+package com.cybertek.bootstrap;
+
+import com.cybertek.entity.User;
+import com.cybertek.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class DataGenerator implements CommandLineRunner {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public DataGenerator(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.userRepository.deleteAll();
+
+        User employee = new User("fs", passwordEncoder.encode("yasa"), "USER", " ");
+        User admin = new User("admin", passwordEncoder.encode("ad123"), "ADMIN", "ACCESS_TEST1,ACCESS_TEST2");
+        User manager = new User("manager", passwordEncoder.encode("man123"), "MANAGER", "ACCESS_TEST1");
+
+        List<User> users = Arrays.asList(employee,admin,manager);
+        userRepository.saveAll(users);
+    }
+
+}
---
+++ Spring-10-Security-HttpBasic-Datasbase/src/main/java/com/cybertek/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.cybertek.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("index.html").permitAll()
+                .antMatchers("/profile/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/management/**").hasAnyRole("MANAGER","ADMIN")
+                .and()
+                .httpBasic(); //perform basic http authentication
+    }
+
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
