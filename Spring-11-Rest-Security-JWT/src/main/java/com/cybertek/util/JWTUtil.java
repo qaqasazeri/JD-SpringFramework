d2a11ba7247e0aacd5cf74f7b4de662b017c57a0
fagansafarov <fagansafarov1985@gmail.com>
2021-02-08 22:07:41 -0600

    Spring Security-JWT

---
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/util/JWTUtil.java
@@ -0,0 +1,41 @@
+package com.cybertek.util;
+
+import com.cybertek.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JWTUtil {
+
+    @Value("${security.jwt.secret-key}")
+    private String secret = "cybertek";
+
+    public String generateToken(User user, String username) {
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username", user.getUsername());
+        claims.put("email", user.getEmail());
+        return createToken(claims, username);
+    }
+
+    private String createToken(Map<String, Object> claims, String username) {
+
+        return Jwts
+                .builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) //10 hours token validity
+                .signWith(SignatureAlgorithm.HS256, secret).compact();
+
+    }
+}
