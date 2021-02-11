56d7f02a44cfa731706572b2532c7660b2115dcd
fagansafarov <fagansafarov1985@gmail.com>
2021-02-10 21:07:02 -0600

    Spring Security-JWT

---
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/entity/AuthenticationRequest.java
@@ -0,0 +1,12 @@
+package com.cybertek.entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AuthenticationRequest {
+    private String username;
+    private String password;
+}
